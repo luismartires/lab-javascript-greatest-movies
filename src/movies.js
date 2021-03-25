df882524b6aa86fe960606dcc4479525ca39339f
Luis Martires <luis.martires@outlook.pt>
2021-03-25 22:17:09 +0000

    missing 4+5+6 plus bonus

--- src/movies.js
+++ src/movies.js
@@ -1,9 +1,30 @@
 // Iteration 1: All directors? - Get the array of all directors.
+function getAllDirectors(movies) {
+  const allDirectors = movies.map((item) => {
+    return item.director;
+  });
+  return allDirectors;
+}
+
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors. How could you "clean" a bit this array and make it unified (without duplicates)?
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
+function howManyMovies(movies) {
+  const spielbergMovies = movies.filter((item) => {
+    return item.director === "Steven Spielberg" && item.genre.includes("Drama");
+  });
+
+  return spielbergMovies.length;
+}
+
 // Iteration 3: All rates average - Get the average of all rates with 2 decimals
+function ratesAverage(movies) {
+  const avgRates = movies.reduce((prev, curr) => {
+    return prev + (curr % 2);
+  });
+  return avgRates();
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 
